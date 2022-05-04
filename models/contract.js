5e913a102be88add154bf902fb7b67f4dec942e3
sarahpicard <sarahannrey225@gmail.com>
2022-05-04 15:10:25 -0400

    add basic contract model

---
+++ models/contract.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const contractSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  saleDate: { type: String }, 
+  entName: { type: String },
+  businessName: { type: String },
+  buyerFirstName: { type: String },
+  buyerLastName: { type: String },
+  natcomNum: { type: String },
+  digicelNum: { type: String },
+  why: { type: String },
+  productPurchased: { type: String },
+  productNum: { type: String },
+  paymentType: { type: String },
+  totalPaid: { type: String },
+  amountToBePaid: { type: String },
+  numberOfPayments: { type: String },
+  amountPerPayment: { type: String },
+  dateOfSignature: { type: String },
+}, {
+  timestamps: true
+})
+
+const Contract = mongoose.model('Profile', contractSchema)
+
+export { Contract }
